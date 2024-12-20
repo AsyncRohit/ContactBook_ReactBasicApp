f9d1add26e1d8791db81262919ea976e7a5bb63f
Rohit <heartstealarrohitthakur@gmail.com>
2024-12-20 22:51:27 +0530

    all contact delete feature

--- src/App.jsx
+++ src/App.jsx
@@ -8,30 +8,49 @@ const App = () => {
   const [favorite, setFavorite] = useState(false)
 
   const [allContact, setAllContact] = useState([]);
+
+
+
   const submitHandler = (e) => {
-    e.preventDefault();
+    e.preventDefault();      // prevent default nature of form 
+
+    //ensuring that form is not empty by trimming it 
     const trimmedName=name.trim()
     const trimmedCompany=company.trim()
     const trimmedNumber=number.trim()
+
+//if form is not empty than add contact to allcontact 
 
     if(trimmedCompany && trimmedName && trimmedNumber ){
       const copyarr=[...allContact,{name,company,number,favorite}]
       setAllContact(copyarr);
     }
+
+    // otherwise alert
     else{
       alert('fill full form')
     }
+
+    //form empty after each submmition 
+
     setName("");
     setCompany("");
     setNumber("");
     setFavorite(false)
   };
 
+  //for deleting any contact
+
   const deleteHandler=(index)=>{
     const copyarr=[...allContact];
     copyarr.splice(index,1)
     setAllContact(copyarr)
+  }
 
+  // for deleting all contact
+
+  const deleteAll =()=>{
+setAllContact([]);
   }
   return (
     <div className=""> 
@@ -85,7 +104,7 @@ const App = () => {
         <input 
         className="mr-2" 
         checked={favorite} 
-        onChange={(e)=> setFavorite(e.target.value)}
+        onChange={(e)=> setFavorite(e.target.checked)}
         type="checkbox"  /> 
         <h1 
         className="inline-block text-xl">
@@ -96,18 +115,26 @@ const App = () => {
         
       
       </form>
-      <div className="px-4 bg-gray-400 h-screen w-full py-2">
-        <h2 className="text-2xl  border-2 border-blue-500 bg-blue-500 text-white text-semi-bold mt-4 w-[13%] px-4 py-2 text-center rounded-md"> All contacts</h2>
-          {allContact.length > 0 ?(
+      <div className="px-4 bg-gray-800 h-screen w-full py-2 ">
+        <div className="w-full gap-10 flex ">
+        <h2 className="text-2xl  border-2 border-blue-500 bg-blue-500 text-white text-semi-bold mt-4 w-[13%]  py-2 text-center rounded-md ml-2 mb-4"> All contacts</h2>
+          <button 
+          onClick={()=>{
+            deleteAll()
+          }}
+          className=" bg-red-700 rounded-md py-2 text-2xl mt-4 ml-2 mb-4 text-white w-[13%]">🗑️ Delete All</button>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-2 mx-auto">
+        {allContact.length > 0 ?(
             allContact.map((elem,index)=>(
-              <div key={index} className="px-10 py-4 bg-gray-300 inline-block w-[30%] rounded-md mr-4 mt-2 relative cursor-pointer ">
+              <div key={index} className="px-10 py-4 bg-gray-300 inline-block  rounded-md mr-4 mt-2 relative cursor-pointer  ">
             <h1 className="text-3xl capitalize font-bold">{elem.name}</h1>
             <h1 className="text-xl capitalize font-semibold ">{elem.company}</h1>
             <h1 className="text-xl">{elem.number}</h1>
             {elem.favorite ?(
-              <p className="text-red-700 absolute right-2 top-2 text-3xl">❤️ </p>
+              <p className="text-red-700 absolute right-2 top-2 text-3xl">❤️ </p>         //if  checked=true
             ):(
-              <p className="text-red-700 absolute right-2 top-2 text-3xl">🤍 </p>
+              <p className="text-red-700 absolute right-2 top-2 text-3xl">🤍 </p>         //otherwise show this 
 
             )}
             <button 
@@ -118,10 +145,12 @@ const App = () => {
           </div>
             ))
           ):(
-            <h2 className="text-2xl text-semi-bold mt-4 text-gray-100">Add contacts...</h2>
+            <h2 className="text-2xl text-semi-bold mt-4 text-gray-100">Contact Book is Empty..</h2>
 
           )}
         
+        </div>
+      
 
         </div>
     </div>
